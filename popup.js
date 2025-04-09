ee40329b85f3ef2ad3ac5718e26758c8c7ca310d
rui.zhang <rui.zhang@tendcloud.com>
2025-04-09 23:03:58 +0800

    init

--- popup.js
+++
@@ -1,6 +0,0 @@
-// Popup script
-chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
-  if (message.action === 'openNavigation') {
-    window.location.href = 'navigation.html';
-  }
-});
